8b6d74893cdf1107926d2f5ecf9b7162f54e5231
Jayesh Waghmare <76690383+Jaye5h@users.noreply.github.com>
2021-05-25 15:18:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import PyPDF2
 book = open('htwfaip.pdf','rb')
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
-print(pages)
+
 speaker = pyttsx3.init()
 for num in range(24,pages):
     page = pdfReader.getPage(24)
